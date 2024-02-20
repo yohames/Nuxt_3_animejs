72d00963bdea2f8d1a5783f7b4e3ff469b44eb60
jonimes <jonimsfn@gmail.com>
2024-02-20 08:34:59 +0300

    update on navigation

---
+++ pages/scroll.vue
@@ -0,0 +1,12 @@
+<style scoped></style>
+<template>
+  <div class="m-20">
+    <NuxtLink
+      to="/"
+      class="bg-gray-100 shadow-lg flex justify-center ring-1 ring-gray-300 hover:bg-gray-200 duration-300 items-center self-start w-40 px-5 rounded-lg py-2"
+      >Back</NuxtLink
+    >
+  </div>
+</template>
+
+<script setup></script>
